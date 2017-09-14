49f2640aca336205a479314aff399297eb851f42
quanganhnv <qa@techmasters-iMac-7.local>
2017-09-14 11:44:41 +0700

    Calculator

---
+++ Calculator/js.js
@@ -0,0 +1,13 @@
+function back() {
+		var value = document.getElementById ("a").value;
+		document.getElementById("a").value= value.substr(0,value.length -1);
+}
+	function binhphuong(){
+		a.value= a.value*a.value;
+		x= a.value; 
+	}
+
+	function can(){
+		a.value = Math.sqrt(a.value)
+		x = a.value
+	}
